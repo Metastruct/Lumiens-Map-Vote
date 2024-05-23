574afc1bd04a97d72000d09897c990faa7ce4b51
TW1STaL1CKY <tw1stal1cky@gmail.com>
2024-05-23 22:02:53 +0100

    Fixed ply error

--- lua/aowl/commands/server/rtv.lua
+++ lua/aowl/commands/server/rtv.lua
@@ -36,7 +36,7 @@ function RTV.Start()
     end
 end
 
-function RTV.AddVote(ply)
+function RTV.AddVote(pl)
     if not RTV.CanVote(pl) then return end
 
     RTV.TotalVotes = RTV.TotalVotes + 1
@@ -63,8 +63,8 @@ function RTV.AddVote(ply)
     end
 end
 
-hook.Add("PlayerDisconnected", "Remove RTV", function(ply)
-    if ply.RTVoted then
+hook.Add("PlayerDisconnected", "Remove RTV", function(pl)
+    if pl.RTVoted then
         RTV.RemoveVote()
     end
 
@@ -75,31 +75,31 @@ hook.Add("PlayerDisconnected", "Remove RTV", function(ply)
     end)
 end)
 
-function RTV.CanVote(ply)
-    local plyCount = table.Count(player.GetAll())
+function RTV.CanVote(pl)
+    local plCount = table.Count(player.GetAll())
     if RTV._ActualWait >= CurTime() then return false, "You must wait a bit before voting!" end
     if GetGlobalBool("In_Voting") then return false, "There is currently a vote in progress!" end
-    if ply.RTVoted then return false, "You have already voted to Rock the Vote!" end
+    if pl.RTVoted then return false, "You have already voted to Rock the Vote!" end
     if RTV.ChangingMaps then return false, "There has already been a vote, the map is going to change!" end
-    if plyCount < RTV.PlayerCount then return false, "You need more players before you can rock the vote!" end
+    if plCount < RTV.PlayerCount then return false, "You need more players before you can rock the vote!" end
 
     return true
 end
 
-function RTV.StartVote(ply)
-    local can, err = RTV.CanVote(ply)
+function RTV.StartVote(pl)
+    local can, err = RTV.CanVote(pl)
 
     if not can then
-        ply:PrintMessage(HUD_PRINTTALK, err)
+        pl:PrintMessage(HUD_PRINTTALK, err)
 
         return
     end
 
-    RTV.AddVote(ply)
+    RTV.AddVote(pl)
 end
 
 concommand.Add("rtv_start", RTV.StartVote)
 
-aowl.AddCommand("rtv", "Rock to vote (vote for next map!)", function(ply)
-    RTV.StartVote(ply)
+aowl.AddCommand("rtv", "Rock to vote (vote for next map!)", function(pl)
+    RTV.StartVote(pl)
 end, "players", true)
